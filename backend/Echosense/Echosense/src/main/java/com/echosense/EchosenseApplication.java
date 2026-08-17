7ff694e19f7d9592b23e2ca3c8ffdbcbc455fddf
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-08-17 10:08:38 -0300

    Add frameworks spring e react

---
+++ backend/Echosense/Echosense/src/main/java/com/echosense/EchosenseApplication.java
@@ -0,0 +1,13 @@
+package com.echosense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchosenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchosenseApplication.class, args);
+	}
+
+}
---
+++ backend/Echosense/Echosense/src/test/java/com/echosense/EchosenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echosense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchosenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
