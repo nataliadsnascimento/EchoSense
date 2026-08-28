649537b80a544a69ce36ec418579f278fc7290a1
Jamili Martins de Oliveira <milimartins133@gmail.com>
2026-08-28 10:19:12 -0300

    adicionando estrutura basica para tratamento de excecoes

---
+++ backend/Echosense/Echosense/src/main/java/com/echosense/exception/StandardError.java
@@ -0,0 +1,19 @@
+package com.echosense.exception;
+
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError {
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
