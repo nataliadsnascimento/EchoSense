aaa2412c40f422d20716c61805c65b49976dfeb3
Natalia Nascimento <nataliasanntos161@gmail.com>
2026-09-13 22:10:09 -0300

    Validação do tiposom e nívelConfiança

--- backend/Echosense/Echosense/src/main/java/com/echosense/dto/EventoSonoroRequestdto.java
+++ backend/Echosense/Echosense/src/main/java/com/echosense/dto/EventoSonoroRequestdto.java
@@ -1,14 +1,23 @@
 package com.echosense.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public class EventoSonoroRequestdto {
 
-    private String tipoSom;
+    @NotNull(message = "O nível de confiança é obrigatório")
+    @Min(value = 0, message = "O nível mínimo de confiança é 0")
+    @Max(value = 100, message = "O nível máximo de confiança é 100")
     private Double nivelConfianca;
+
     private LocalDateTime dataHora;
 
-    public EventoSonoroRequestdto() {}
+    @NotBlank(message = "O tipo de som não pode estar vazio")
+    private String tipoSom;
 
     public String getTipoSom() {
         return tipoSom;
