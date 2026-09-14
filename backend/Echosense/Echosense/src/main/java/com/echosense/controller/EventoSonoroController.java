13a15276ec69a4a6b4ecf68448e08a03621922c9
Natalia Nascimento <nataliasanntos161@gmail.com>
2026-09-13 22:37:13 -0300

    Implementa os endpoints get e delete

--- backend/Echosense/Echosense/src/main/java/com/echosense/controller/EventoSonoroController.java
+++ backend/Echosense/Echosense/src/main/java/com/echosense/controller/EventoSonoroController.java
@@ -6,6 +6,9 @@ import com.echosense.service.EventoSonoroService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/eventos")
@@ -18,8 +21,26 @@ public class EventoSonoroController {
     }
 
     @PostMapping
-    public ResponseEntity<EventoSonoro> registrarEvento(@RequestBody EventoSonoroRequestdto dto) {
+    public ResponseEntity<EventoSonoro> registrarEvento(@RequestBody @Valid EventoSonoroRequestdto dto) {
         EventoSonoro eventoSalvo = eventoSonoroService.registrarEvento(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(eventoSalvo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventoSonoro> buscarporId(@PathVariable Long id){
+        EventoSonoro evento = eventoSonoroService.buscarPorId(id);
+        return ResponseEntity.ok(evento);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventoSonoro>> listarHistorico(){
+        List<EventoSonoro> historico = eventoSonoroService.listarHistorico();
+        return ResponseEntity.ok(historico);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirEvento(@PathVariable Long id){
+        eventoSonoroService.excluir(id);
+        return ResponseEntity.noContent().build();
     }
 }
