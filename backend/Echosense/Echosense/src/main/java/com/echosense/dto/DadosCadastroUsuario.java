9ad737012214de092ebf9c0b46937bf9ec4fcdc1
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-09-20 20:42:24 -0300

    Dto para validação dos dados de entrada (min = 6)

---
+++ backend/Echosense/Echosense/src/main/java/com/echosense/dto/DadosCadastroUsuario.java
@@ -0,0 +1,18 @@
+package com.echosense.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DadosCadastroUsuario(
+        @NotBlank(message = "O nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+        String senha
+) {}
